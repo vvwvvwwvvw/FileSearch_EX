1547c603f1039b380419ff5013c96f8c487c0226
남정욱 <wjddnr3304@gmail.com>
2024-10-24 14:55:01 +0900

    파일 이동 추가

--- FileSearch_EX/Form1.Designer.cs
+++ FileSearch_EX/Form1.Designer.cs
@@ -35,18 +35,23 @@
             this.button2 = new System.Windows.Forms.Button();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 10);
+            this.textBox1.Location = new System.Drawing.Point(12, 32);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(575, 25);
             this.textBox1.TabIndex = 0;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(604, 12);
+            this.button1.Location = new System.Drawing.Point(604, 32);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 1;
@@ -62,7 +67,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(604, 53);
+            this.button2.Location = new System.Drawing.Point(604, 96);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 3;
@@ -72,7 +77,7 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(12, 54);
+            this.textBox2.Location = new System.Drawing.Point(12, 97);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(575, 25);
             this.textBox2.TabIndex = 4;
@@ -81,17 +86,67 @@
             // 
             this.listBox1.FormattingEnabled = true;
             this.listBox1.ItemHeight = 15;
-            this.listBox1.Location = new System.Drawing.Point(12, 108);
+            this.listBox1.Location = new System.Drawing.Point(12, 141);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(575, 454);
+            this.listBox1.Size = new System.Drawing.Size(575, 379);
             this.listBox1.TabIndex = 5;
             this.listBox1.DoubleClick += new System.EventHandler(this.listBox1_DoubleClick);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(604, 497);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Send";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 15);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "경로";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 15);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "검색어";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 538);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(575, 25);
+            this.textBox3.TabIndex = 9;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(604, 540);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "...";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // test
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(691, 604);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.button2);
@@ -112,6 +167,11 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- FileSearch_EX/Form1.cs
+++ FileSearch_EX/Form1.cs
@@ -28,62 +28,134 @@ namespace FileSearch_EX
         }
 
         // 파일 검색 및 내용 확인
-        private async void button2_Click(object sender, EventArgs e)
+        public async void button2_Click(object sender, EventArgs e)
         {
             button2.Enabled = false;
 
-            string searchDirectory = textBox1.Text;  // 파일을 검색할 경로
-            string searchTerm = textBox2.Text;       // 검색어
+            string searchDirectory = textBox1.Text;
+            string searchTerm = textBox2.Text;
             List<string> resultFiles = new List<string>();
 
-            if (string.IsNullOrWhiteSpace(searchDirectory) || string.IsNullOrWhiteSpace(searchTerm))
+            if (string.IsNullOrWhiteSpace(searchDirectory))
             {
-                MessageBox.Show("검색어를 입력해주세요.");
+                MessageBox.Show("경로를 선택해 주세요.");
                 button2.Enabled = true;
                 return;
             }
             await Task.Run(() =>
             {
-                var files = Directory.GetFiles(searchDirectory, "*.txt", SearchOption.AllDirectories).ToList();
-
-                files.ForEach(file =>
+                try
                 {
-                    // 파일 내용을 읽어서 검색어가 포함된 파일을 필터링
-                    string fileContent = File.ReadAllText(file);
+                    var files = string.IsNullOrWhiteSpace(searchTerm)
+                        ? FastDirectoryEnumerator.DirectorySearch(searchDirectory, "*.*", 0).ToList() // 모든 파일 검색
+                        : FastDirectoryEnumerator.DirectorySearch(searchDirectory, "*.txt", 0).ToList(); // 텍스트 파일만 검색
 
-                    if (fileContent.Contains(searchTerm))
+                    files.ForEach(file =>
                     {
-                        resultFiles.Add(file);  // 검색어가 포함된 파일 추가
-                    }
-                });
+                        try
+                        {
+                            string fileName = file.FullName;
+
+                            if (!string.IsNullOrWhiteSpace(searchTerm))
+                            {
+                                string fileContent = File.ReadAllText(fileName);
+                                if (fileContent.Contains(searchTerm))
+                                {
+                                    resultFiles.Add(fileName);  // 검색어가 포함된 파일만 리스트에 추가
+                                }
+                            }
+                            else
+                            {
+                                resultFiles.Add(fileName);
+                            }
+                        }
+                        catch
+                        {
+                  
+                        }
+                    });
+                }
+                catch
+                {
+        
+                }
             });
+
             this.Invoke((MethodInvoker)delegate
             {
-                listBox1.Items.Clear();  // 기존 리스트 초기화
+                listBox1.Items.Clear();
+
                 if (resultFiles.Count > 0)
                 {
-                    listBox1.Items.AddRange(resultFiles.ToArray());  // 결과 파일 리스트에 추가
+                    listBox1.Items.AddRange(resultFiles.ToArray());
                 }
                 else
                 {
                     MessageBox.Show("검색어가 포함된 파일이 없습니다.");
                 }
-            });
 
-            button2.Enabled = true;
+                button2.Enabled = true;
+            });
         }
 
         private void listBox1_DoubleClick(object sender, EventArgs e)
         {
             Process ExternalProcess = new Process();
             // 실행파일 경로
-            ExternalProcess.StartInfo.FileName = listBox1.Items[0].ToString();
+            ExternalProcess.StartInfo.FileName = listBox1.SelectedItem.ToString();
             // 실행시킬 파일 크기
             ExternalProcess.StartInfo.WindowStyle = ProcessWindowStyle.Normal;
-            //Process 시작
+            // Process 시작
             ExternalProcess.Start();
             ExternalProcess.WaitForExit();
+        }
 
+        private void button4_Click(object sender, EventArgs e)
+        {
+            BetterFolderBrowser ex = new BetterFolderBrowser();
+            ex.Multiselect = false;
+            if (ex.ShowDialog() == DialogResult.OK)
+            {
+                this.textBox3.Text = ex.SelectedPath;  // 파일 검색 경로
+            }
+        }
+
+        private async void button3_Click(object sender, EventArgs e)
+        {
+            button3.Enabled = false;
+
+            string targetPath = textBox3.Text;
+            string selectedFile = listBox1.SelectedItem?.ToString(); // 선택된 파일 가져오기
+
+            if (string.IsNullOrWhiteSpace(targetPath))
+            {
+                MessageBox.Show("경로를 선택해 주세요.");
+                button3.Enabled = true;
+                return;
+            } else if (string.IsNullOrWhiteSpace(selectedFile))
+            {
+                MessageBox.Show("이동할 파일을 선택해 주세요");
+                button3.Enabled = true;
+                return;
+            }
+
+            await Task.Run(() =>
+            {
+                try
+                {
+                    // 파일 이동
+                    string fileName = Path.GetFileName(selectedFile);
+                    string destinationPath = Path.Combine(targetPath, fileName);
+                    File.Move(selectedFile, destinationPath);
+                    MessageBox.Show("파일을 이동하였습니다");
+                    listBox1.Refresh();
+                }
+                catch
+                {
+   
+                }
+            });     
+            button3.Enabled = true;
         }
     }
 }
